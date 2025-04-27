907b1f3bb829a8eb17e6c7d5cd54752287f2fd45
BorodinRoman <borodinroman966@gmail.com>
2025-04-27 22:37:13 +1000

    add post-request

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,28 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from app import app
 
 
 @app.route("/")
-def home():
-    return render_template("index.html")
+def form():
+    return render_template("form.html")
 
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+@app.route("/submit", methods=["POST", "GET"])
+def submit():
+    if request.method == "POST":
+        data = {
+            "name": request.form.get("name"),
+            "email": request.form.get("email"),
+            "color": request.form.get("color"),
+            "profession": request.form.get("profession"),
+            "hobbies": request.form.getlist("hobbies"),
+            "level": request.form.get("level"),
+        }
+
+        return render_template("result.html", **data)
+    return redirect(url_for("form"))
 
 
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
+@app.route("/result")
+def result():
+    return render_template("result.html")
