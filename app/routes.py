002e318e243c1d2794f533742bda3735276c503b
BorodinRoman <borodinroman966@gmail.com>
2025-04-30 20:41:57 +1000

    return templates from commit 3231998c030e36568840c531d80c8be6ed78ca23

--- app/routes.py
+++ app/routes.py
@@ -1,28 +1,17 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template
 from app import app
 
 
 @app.route("/")
-def form():
-    return render_template("form.html")
+def home():
+    return render_template("index.html")
 
 
-@app.route("/submit", methods=["POST", "GET"])
-def submit():
-    if request.method == "POST":
-        data = {
-            "name": request.form.get("name"),
-            "email": request.form.get("email"),
-            "color": request.form.get("color"),
-            "profession": request.form.get("profession"),
-            "hobbies": request.form.getlist("hobbies"),
-            "level": request.form.get("level"),
-        }
-
-        return render_template("result.html", **data)
-    return redirect(url_for("form"))
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 
-@app.route("/result")
-def result():
-    return render_template("result.html")
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
